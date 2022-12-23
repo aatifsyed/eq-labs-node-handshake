ddec6da1b94777cdd4feb9387fc689eb066ed99c
Aatif Syed <aatif@aatifsyed.uk>
2022-12-23 14:12:42 +0000

    feat: can deserialize version

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,60 @@
-use nom_derive::Parse;
-
 /// https://en.bitcoin.it/wiki/Protocol_documentation
 pub mod wire {
+    use tap::Pipe;
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct VarInt {
+        pub inner: u64,
+    }
+
+    impl<'a, E> nom_derive::Parse<&'a [u8], E> for VarInt
+    where
+        E: nom::error::ParseError<&'a [u8]>,
+    {
+        fn parse(initial_input: &'a [u8]) -> nom::IResult<&'a [u8], Self, E> {
+            let (rem, i) = {
+                let (rem, first_byte) = u8::parse(initial_input)?;
+                match first_byte {
+                    // 0xFF followed by the length as uint64_t
+                    0xFF => u64::parse_le(rem)?,
+                    // 0xFE followed by the length as uint32_t
+                    0xFE => u32::parse_le(rem)?.pipe(|(rem, i)| (rem, i as u64)),
+                    // 0xFD followed by the length as uint16_t
+                    0xFD => u16::parse_le(rem)?.pipe(|(rem, i)| (rem, i as u64)),
+                    i => (rem, i as u64),
+                }
+            };
+            Ok((rem, VarInt { inner: i }))
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    pub struct VarStr {
+        pub inner: String,
+    }
+
+    impl<'a, E> nom_derive::Parse<&'a [u8], E> for VarStr
+    where
+        E: nom::error::ParseError<&'a [u8]>
+            + nom::error::FromExternalError<&'a [u8], std::str::Utf8Error>,
+    {
+        fn parse(initial_input: &'a [u8]) -> nom::IResult<&'a [u8], Self, E> {
+            let (rem, length) = VarInt::parse(initial_input)?;
+            let (rem, s) = nom::combinator::map_res(
+                // should fail to compile on 32-bit platforms, as nom::traits::ToUsize isn't implemented for u64 on those platforms
+                // so we should be arithmetically safe
+                nom::bytes::streaming::take(length.inner),
+                std::str::from_utf8,
+            )(rem)?;
+            Ok((
+                rem,
+                Self {
+                    inner: String::from(s),
+                },
+            ))
+        }
+    }
+
     #[derive(Debug, nom_derive::Nom, Clone, Copy, PartialEq, Eq, Hash)]
     #[repr(C)]
     pub struct Header {
@@ -13,13 +66,14 @@ pub mod wire {
         #[nom(LittleEndian)]
         pub length: u32,
         /// First 4 bytes of sha256(sha256(payload)).
-        pub checksum: u32,
+        pub checksum: [u8; 4],
     }
 
     #[derive(Debug, nom_derive::Nom, Clone, Copy, PartialEq, Eq, Hash)]
     #[repr(C)]
     pub struct NetworkAddressWithoutTimestamp {
         /// See [Version::services].
+        #[nom(LittleEndian)]
         pub services: u64,
         /// Network byte order.
         /// The original client only supported IPv4 and only read the last 4 bytes to get the IPv4 address.
@@ -31,19 +85,47 @@ pub mod wire {
         pub port: u16,
     }
 
-    #[derive(Debug, nom_derive::Nom, Clone, Copy, PartialEq, Eq, Hash)]
+    #[derive(Debug, nom_derive::Nom, Clone, PartialEq, Eq, Hash)]
     #[repr(C)]
     pub struct Version {
         /// Identifies protocol version being used by the node.
         #[nom(LittleEndian)]
         pub version: u32,
         /// Bitfield of features to be enabled for this connection.
+        #[nom(LittleEndian)]
         pub services: u64,
         /// Standard UNIX timestamp in seconds.
         #[nom(LittleEndian)]
         pub timestamp: u64,
-        /// The network address of the node receiving this message
+        /// The network address of the node receiving this message.
         pub addr_recv: NetworkAddressWithoutTimestamp,
+        /// Field can be ignored.
+        /// This used to be the network address of the node emitting this message, but most P2P implementations send 26 dummy bytes.
+        /// The "services" field of the address would also be redundant with the second field of the version message.
+        pub addr_from: NetworkAddressWithoutTimestamp,
+        /// Node random nonce, randomly generated every time a version packet is sent. This nonce is used to detect connections to self.
+        pub nonce: u64,
+        /// User Agent (0x00 if string is 0 bytes long)
+        pub user_agent: VarStr,
+        /// The last block received by the emitting node
+        #[nom(LittleEndian)]
+        pub start_height: u32,
+        /// Whether the remote peer should announce relayed transactions or not, see BIP 0037
+        #[nom(Parse = "parse_bool")]
+        pub relay: bool,
+    }
+
+    fn parse_bool(initial_input: &[u8]) -> nom::IResult<&[u8], bool> {
+        let (rem, byte) = <u8 as nom_derive::Parse<&[u8]>>::parse(initial_input)?;
+        match byte {
+            1 => Ok((rem, true)),
+            0 => Ok((rem, false)),
+            // TODO(aatifsyed) plumb the errors here properly
+            _ => Err(nom::Err::Error(nom::error::Error::new(
+                initial_input,
+                nom::error::ErrorKind::Alt,
+            ))),
+        }
     }
 
     #[cfg(test)]
@@ -54,13 +136,21 @@ pub mod wire {
         #[test]
         fn example_header() {
             let s = concat!(
-                "F9 BE B4 D9 76 65 72 73  69 6F 6E 00 00 00 00 00", // ....version.....
-                "55 00 00 00 9C 7C 00 00  01 00 00 00 00 00 00 00", // U....|..........
-                "E6 15 10 4D 00 00 00 00  01 00 00 00 00 00 00 00", // ...M............
-                "00 00 00 00 00 00 00 00  00 00 FF FF 0A 00 00 01", // ................
-                "20 8D 01 00 00 00 00 00  00 00 00 00 00 00 00 00", // ................
-                "00 00 00 00 FF FF 0A 00  00 02 20 8D DD 9D 20 2C", // .......... ... ,
-                "3A B4 57 13 00 55 81 01  00                     ", // :.W..U...
+                // Message Header:
+                "F9 BE B4 D9",                         // - Main network magic bytes
+                "76 65 72 73 69 6F 6E 00 00 00 00 00", // - "version" command
+                "64 00 00 00",                         // - Payload is 100 bytes long
+                "35 8d 49 32",                         // - payload checksum (internal byte order)
+                // Version message:
+                "62 EA 00 00",             // - 60002 (protocol version 60002)
+                "01 00 00 00 00 00 00 00", // - 1 (NODE_NETWORK services)
+                "11 B2 D0 50 00 00 00 00", // - Tue Dec 18 10:12:33 PST 2012
+                "01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00 00 00", // - Recipient address info - see Network Address
+                "01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00 00 00", // - Sender address info - see Network Address
+                "3B 2E B3 5D 8C E6 17 65", // - Node ID
+                "0F 2F 53 61 74 6F 73 68 69 3A 30 2E 37 2E 32 2F                              ", // - "/Satoshi:0.7.2/" sub-version string (string is 15 bytes long)
+                "C0 3E 03 00", // - Last block sending node has is block #212672
+                "01",          // - relay
             )
             .chars()
             .filter(char::is_ascii_alphanumeric)
@@ -72,10 +162,36 @@ pub mod wire {
                 Header {
                     magic: crate::constants::Magic::Main as u32,
                     command: *b"version\0\0\0\0\0",
-                    length: 85,
-                    checksum: 0
+                    length: 100,
+                    checksum: [0x35, 0x8d, 0x49, 0x32],
                 }
-            )
+            );
+            let (rest, version) = Version::parse(&rest).unwrap();
+            assert_eq!(
+                version,
+                Version {
+                    version: 60002,
+                    services: crate::constants::Services::NodeNetwork as _,
+                    timestamp: 1355854353,
+                    addr_recv: NetworkAddressWithoutTimestamp {
+                        services: crate::constants::Services::NodeNetwork as _,
+                        ipv6_address: std::net::Ipv4Addr::UNSPECIFIED.to_ipv6_mapped().octets(),
+                        port: 0
+                    },
+                    addr_from: NetworkAddressWithoutTimestamp {
+                        services: crate::constants::Services::NodeNetwork as _,
+                        ipv6_address: std::net::Ipv4Addr::UNSPECIFIED.to_ipv6_mapped().octets(),
+                        port: 0
+                    },
+                    nonce: u64::from_be_bytes([0x3B, 0x2E, 0xB3, 0x5D, 0x8C, 0xE6, 0x17, 0x65]),
+                    user_agent: VarStr {
+                        inner: String::from("/Satoshi:0.7.2/")
+                    },
+                    start_height: 212672,
+                    relay: true
+                }
+            );
+            assert_eq!(rest.len(), 0);
         }
     }
 }
